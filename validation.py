16b3ed5b9c72faece2488cb1cde5b66c4d79db26
Sumit Satyam <sumitsatyam2611@gmail.com>
2024-02-05 15:21:09 +0530

    adding validation python file for validating python functions

---
+++ validation.py
@@ -0,0 +1,41 @@
+'''num = int(input("Please enter any number to check"))
+
+
+def validate(num):
+    if num % 2 == 0:
+        print("Number is even")
+    else:
+        print("Number is odd")
+    return(num)
+
+
+print("Provided number is",validate(num))
+'''
+
+'''
+def palindrom_check(str):
+    return str == str[::-1]
+
+
+##Driver code
+str = 'radar'
+ans = palindrom_check(str)
+
+if ans:
+    print("String is palindrome")
+
+else:
+    print("String is not palindrome")
+'''
+'''
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+number = 5
+result = factorial(number)
+print(f"factorial of {number} is {result}")
+
+'''
